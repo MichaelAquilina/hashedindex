7f50686be2c2a6dfda0fee85dc76973ce8f21e90
Michael Aquilina <michaelaquilina@gmail.com>
2015-06-05 14:59:53 +0100

    Fix requirements in setup.py

--- setup.py
+++ setup.py
@@ -13,8 +13,10 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
-with open('requirements.txt') as requirements_file:
-    requirements = requirements_file.read().split('\n')
+requirements  = [
+    'pandas',
+    'numpy',
+]
 
 test_requirements = [
     'pytest',
