dd8dd054ac0fc2030bec01d442b37a7559979428
Michael Aquilina <michaelaquilina@gmail.com>
2015-07-07 14:48:40 +0100

    Bump up version to 0.2.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='hashedindex',
-    version='0.2.1',
+    version='0.2.2',
     description="InvertedIndex implementation using hash lists (dictionaries)",
     long_description=readme + '\n\n' + history,
     author="Michael Aquilina",
