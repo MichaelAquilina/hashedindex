cfc19df6a84f2b2f3fedb675b668f9f999f63a9c
Michael Aquilina <michaelaquilina@gmail.com>
2015-07-13 20:42:38 +0100

    Add textparser module for generating tokens from text

---
+++ hashedindex/textparser.py
@@ -0,0 +1,103 @@
+# -*- encoding: utf8 -*-
+from __future__ import unicode_literals, print_function, division
+
+import re
+import math
+import unicodedata
+
+from copy import copy
+from string import ascii_letters, digits, punctuation
+
+
+# Stemmer interface which returns token unchanged
+class NullStemmer(object):
+
+    def stem(self, x):
+        return x
+
+    def __str__(self):
+        return '<NullStemmer>'
+
+
+_stopwords = frozenset()
+_accepted = frozenset(ascii_letters + digits + punctuation) - frozenset('\'')
+
+_punctuation = copy(punctuation)
+_punctuation = _punctuation.replace('\\', '')
+_punctuation = _punctuation.replace('/', '')
+_punctuation = _punctuation.replace('-', '')
+
+_re_punctuation = re.compile('[%s]' % re.escape(_punctuation))
+_re_token = re.compile(r'[a-z0-9]+')
+
+_url_pattern = r'(https?:\/\/)?(([\da-z-]+)\.){1,2}.([a-z\.]{2,6})(/[\/\w \.-]*)*\/?(\?(\w+=\w+&?)+)?'
+_re_full_url = re.compile(r'^%s$' % _url_pattern)
+_re_url = re.compile(_url_pattern)
+
+
+# Determining the best way to calculate tfidf is proving difficult, might need more advanced techniques
+def tfidf(tf, df, corpus_size):
+    if df and tf:
+        return (1 + math.log(tf)) * math.log(corpus_size / df)
+    else:
+        return 0
+
+
+def normalize_unicode(text):
+    """
+    Normalize any unicode characters to ascii equivalent
+    https://docs.python.org/2/library/unicodedata.html#unicodedata.normalize
+    """
+    if type(text) == unicode:
+        return unicodedata.normalize('NFKD', text).encode('ascii', 'ignore')
+    else:
+        return text
+
+
+def get_ngrams(token_list, n=2):
+    for i in range(len(token_list) - n + 1):
+        yield token_list[i:i+n]
+
+
+def word_tokenize(text, stopwords=_stopwords, ngrams=None, min_length=3, ignore_numeric=True):
+    """
+    Parses the given text and yields tokens which represent words within
+    the given text. Tokens are assumed to be divided by any form of
+    whitespace character.
+    """
+    if ngrams is None:
+        ngrams = 1
+
+    text = re.sub(re.compile('\'s'), '', text)  # Simple heuristic
+    text = re.sub(_re_punctuation, '', text)
+
+    matched_tokens = re.findall(_re_token, text.lower())
+    for tokens in get_ngrams(matched_tokens, ngrams):
+        for i in range(len(tokens)):
+            tokens[i] = tokens[i].strip(punctuation)
+
+            if len(tokens[i]) < min_length or tokens[i] in stopwords:
+                break
+            if ignore_numeric and isnumeric(tokens[i]):
+                break
+        else:
+            yield tuple(tokens)
+
+
+def isnumeric(text):
+    """
+    Returns a True if the text is purely numeric and False otherwise.
+    """
+    try:
+        float(text)
+    except ValueError:
+        return False
+    else:
+        return True
+
+
+def is_url(text):
+    """
+    Returns a True if the text is a url and False otherwise.
+    """
+    return bool(_re_full_url.match(text))
---
+++ tests/test_parser.py
@@ -0,0 +1,108 @@
+# -*- encoding: utf8 -*-
+from __future__ import unicode_literals, print_function, division
+
+import unittest
+
+from hashedindex import textparser
+
+
+class TfidfTestCase(unittest.TestCase):
+
+    def test_zero_term_frequency(self):
+        assert textparser.tfidf(tf=0, df=10, corpus_size=1) == 0
+
+    def test_zero_document_frequency(self):
+        assert textparser.tfidf(tf=10, df=0, corpus_size=1) == 0
+
+
+class IsNumericTestCase(unittest.TestCase):
+
+    def test_integer(self):
+        assert textparser.isnumeric('23')
+        assert textparser.isnumeric('8431')
+
+    def test_float(self):
+        assert textparser.isnumeric('23.480')
+        assert textparser.isnumeric('9.6502')
+
+    def test_scientific_notation(self):
+        assert textparser.isnumeric('1e-10')
+        assert textparser.isnumeric('2e+54')
+
+    def test_no_numeric(self):
+        assert not textparser.isnumeric('foo')
+        assert not textparser.isnumeric('10 foo')
+
+
+class GetNGramsTestCase(unittest.TestCase):
+
+    def test_bigram_token_list(self):
+        assert list(textparser.get_ngrams(
+            token_list=['one', 'two', 'three', 'four'],
+        )) == [['one', 'two'], ['two', 'three'], ['three', 'four']]
+
+    def test_trigram_token_list(self):
+        assert list(textparser.get_ngrams(
+            token_list=['one', 'two', 'three', 'four'],
+            n=3,
+        )) == [
+            ['one', 'two', 'three'],
+            ['two', 'three', 'four'],
+        ]
+
+
+class WordTokenizeTestCase(unittest.TestCase):
+
+    def test_sentence(self):
+        assert list(textparser.word_tokenize(
+            text='hello cruel world',
+        )) == [('hello', ), ('cruel', ), ('world', )]
+
+    def test_splits_punctuation(self):
+        assert list(textparser.word_tokenize(
+            text='first. second',
+        )) == [('first', ), ('second', )]
+
+    def test_ignores_stopwords(self):
+        assert list(textparser.word_tokenize(
+            text='The first rule of python is',
+            stopwords=set(['the', 'of', 'is']),
+            min_length=1,
+        )) == [('first', ), ('rule', ), ('python', )]
+
+    def test_min_length(self):
+        assert list(textparser.word_tokenize(
+            text='one for the money two for the go',
+            min_length=4,
+        )) == [('money', )]
+
+    def test_ignores_numeric(self):
+        assert list(textparser.word_tokenize(
+            text='one two 3 four',
+        )) == [('one', ), ('two', ), ('four', )]
+
+    def test_ngrams(self):
+        assert list(textparser.word_tokenize(
+            text='foo bar bomb blar',
+            ngrams=2,
+        )) == [('foo', 'bar'), ('bar', 'bomb'), ('bomb', 'blar')]
+
+
+class IsUrlTestCase(unittest.TestCase):
+
+    def test_http_url(self):
+        assert textparser.is_url('http://www.google.com')
+
+    def test_https_url(self):
+        assert textparser.is_url('https://www.google.com')
+
+    def test_url_with_path(self):
+        assert textparser.is_url('https://www.facebook.com/some/path/here')
+
+    def test_url_with_query_string(self):
+        assert textparser.is_url('https://www.yplanapp.com?foo=1&bar=2')
+
+    def test_not_a_url(self):
+        assert not textparser.is_url('foo')
+        assert not textparser.is_url('bar')
+        assert not textparser.is_url('waterboat')
