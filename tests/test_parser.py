8ff37c629f78c367324bc1f6f9f9682472d93148
James Addison <jay@jp-hosting.net>
2020-04-30 10:18:09 +0100

    Add 'retain_punctuation' argument to tokenizer

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -148,6 +148,7 @@ class WordTokenizeTestCase(unittest.TestCase):
     def test_retains_punctuation(self):
         assert list(textparser.word_tokenize(
             text='who, where? (question)',
+            retain_punctuation=True
         )) == [('who', ), (',', ), ('where',), ('?', ), ('(',), ('question',), (')',)]
 
     def test_ngrams(self):
